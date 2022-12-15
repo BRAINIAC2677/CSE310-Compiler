c9e3152a9842e77e87f3f43cd7670d9b314dc6f1
BRAINIAC2677 <brainiac2677@gmail.com>
2022-12-16 00:49:22 +0600

    offline-2 ongoing

--- offline-1/implementation/build-and-run.sh
+++
@@ -1,5 +0,0 @@
-
-# build
-g++ SymbolInfo.cpp ScopeTable.cpp SymbolTable.cpp main.cpp -o main
-# run
-./main 
---
+++ offline-2/testers/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a = "\\";
+    cout << a.size() << "\n"
+         << a << "\n";
+    return 0;
+}
